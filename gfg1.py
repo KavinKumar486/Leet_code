d093a32bc0b1238631fd664807823be6c0a8114a
KavinKumar486 <kavin45264@gmail.com>
2025-04-06 21:10:16 +0530

    Topological sort

--- gfg1.py
+++ gfg1.py
@@ -1,3 +1,20 @@
-#second largest element 
-nums=[]
-print(max(nums))
+from collections import defaultdict
+class Solution:
+    
+    def topoSort(self, V, edges):
+        visited = [0]*len(edges)
+        res=[]
+        adj=defaultdict(list)
+        def dfs(node):
+            if not visited[node]:
+                visited[node] = 1
+            else:
+                return
+            for i in adj[node]:
+                dfs(i)
+                res.append(node)
+        for i,j in edges:
+            adj[i].append(j)
+        for i in adj:
+            dfs(i)
+        return res
